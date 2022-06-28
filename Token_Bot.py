e0fd648cebde627fafb366b91a128ce96b05f820
gmozg <glebssa88@gmail.com>
2022-06-28 16:24:04 +0300

    Token_Bot.py

---
+++ Token_Bot.py
@@ -0,0 +1,70 @@
+import requests
+import datetime
+from config import token_bot, open_weather_token
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+#import ipdb; ipdb.set_trace()
+bot = Bot(token = token_bot)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=["start"])
+async def start_command(message: types.Message):
+    await message.reply("Хелоушки! Накатай ка мне город и я тебе выдам знания о погоде!")
+
+@dp.message_handler()
+async def get_meather(message: types.Message):
+    code_to_smile = {
+        "Clear": "Ясно \U00002600",
+        "Clouds": "Облачно \U00002601",
+        "Rain": "Дождь \U00002614",
+        "Drizzle": "Дождь \U00002614",
+        "Thunderstorm": "Гроза \U000026A1",
+        "Snow": "Снег \U0001F328",
+        "Mist": "Туман \U0001F32B"
+    }
+
+
+
+    try:
+        r = requests.get(
+        f"https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_weather_token}&units=metric"
+        )
+        data = r.json()
+        #pprint(data)
+
+        # import ipdb; ipdb.set_trace()
+
+        city = data["name"]
+        cur_weather = data["main"]["temp"]
+
+        weather_description = data["weather"][0]["main"]
+
+        if weather_description in code_to_smile:
+            wd = code_to_smile[weather_description]
+        else:
+            wd = "Посмотри в окно , очевидно, я же слепень)" 
+
+        humidity = data["main"]["humidity"]
+        pressure = data["main"]["pressure"]
+        wind = data["wind"]["speed"]
+        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+        length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
+            data["sys"]["sunrise"])
+
+        await message.reply(f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
+            f"Погода в городе: {city}\nТемпература; {cur_weather}C°{wd}\n"
+            f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind}м/с\n"
+            f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunrise_timestamp}\nПродолжительность дня: {length_of_the_day}\n"
+            f"***Хорошего дня!***"
+        )
+
+    except:
+        await message.reply("\U00002620 Проверьте название города \U00002620")    
+
+
+if __name__== '__main__':
+    executor.start_polling(dp)
